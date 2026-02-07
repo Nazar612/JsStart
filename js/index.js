f81150d409134e4d5643b177d30120035e8261f6
Nazar612 <nazarstovba300@gmail.com>
2026-02-07 20:34:01 +0100

    JavaScriptStart

--- js/index.js
+++ js/index.js
@@ -6,7 +6,27 @@
 // const(Ніколи не змінюється), let(Може змінюватись) =відношення 90/10 -var(Застаріла змінна)
 
 const name = "Artem";
-
 console.log(name); 
 
-let age = "number";
+let year = "2026";
+year = "2027"
+console.log (year)
+
+// Типи даних
+
+const string = "jhskgdfhj12223";
+
+const number = 19;
+
+const boolean = true || false;
+
+// const bigInt = 100000000000000000;
+
+// const symbol = @$# ;
+
+const nulled = null; // це рукотворне значення
+
+const undefined = undefined; // це визначення JS коли він пішов чось шукати і не найшов
+
+
+console.log (typeof name); //визначає тип змінної
