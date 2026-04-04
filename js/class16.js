82641a4b954822c452200812c0a1d3dd07195d1c
Nazar612 <nazarstovba300@gmail.com>
2026-04-04 13:00:27 +0200

    JavaScriptStart

--- js/class16.js
+++ js/class16.js
@@ -3,3 +3,47 @@ for(let i = 0; i < 100; i++) {
     emptyMassive.push(i);
 }
 console.log(emptyMassive);
+
+
+const sliceMassive = [1,2,3,4,5,6,7,8,9,10];
+const indexOfTen = sliceMassive.indexOf(1);
+if(indexOfTen !== -1) {
+    const copyArr = sliceMassive.slice(indexOfTen, indexOfTen + 1);
+    console.log(copyArr);
+}
+
+// indexOf працює так:
+
+// let elementNumber = 0;
+// for(let i = 0; i < sliceMassive.length; i++) {
+//     if(number[i] === 10) {
+//         elementNumber = i;
+//     }
+// }
+// console.log(elementNumber);
+
+
+
+const namesMassive = ["Nazar", "Vika", "Taras"];
+const deleteElement = namesMassive.indexOf("Vika");
+console.log(deleteElement);
+if (deleteElement != -1) {
+    console.log(namesMassive.slice(deleteElement, deleteElement + 1));
+}
+
+const nazar = namesMassive.splice(0, 1, "Artem");
+console.log(namesMassive);
+console.log(nazar);
+
+
+
+
+
+
+// .splice() дозволяє відрізати значення
+
+console.log(sliceMassive.splice(indexOfTen, 5, "a","b","c","d","e"));
+console.log(sliceMassive);
+
+
+
---
+++ js/hw8.js
@@ -0,0 +1,38 @@
+//Завдання 1
+// Напиши скрипт, який об'єднує всі елементи массива в один рядок. Елементів може бути довільна кількість. Нехай елементи массива в рядку будут розділені комою.
+// 👉 Спочатку через for
+// 👉 Потім через join()
+
+// const friends = ["Mango", "Poly", "Kiwi", "Ajax"];
+// let string = "";
+// for( let i = 0; i < friends.length; i++) {
+//     const counter = friends[i];
+// }
+// console.log()
+//Я не знаю як це через for зробити 
+
+const friends = ["Mango", "Poly", "Kiwi", "Ajax"];
+let string = friends.join()
+console.log(string);
+
+//Завдання 2
+// Працюємо з колекцією карток в trello. Метод splice() (можна використати інші методи)
+
+const cards = [
+    "Карточка-1",
+    "Карточка-2",
+    "Карточка-3",
+    "Карточка-4",
+    "Карточка-5",
+];
+
+const cardToRemove = cards.indexOf("Карточка-3");
+const removeCart = cards.splice(cardToRemove, cardToRemove -1)
+console.log(cards)
+
+cards.push("Карточка-6");
+console.log(cards);
+
+const cardToUpdate = cards.indexOf("Карточка-4");
+const cardUpdate = cards.splice(cardToUpdate, cardToUpdate -1, "Оновлена Карточка-4");
+console.log(cards)
